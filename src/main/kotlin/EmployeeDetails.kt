9813501719baf4b9e5ce9a30db76c830dea1a9ca
sridharpanda1 <sridharpanda.work@gmail.com>
2026-01-05 23:45:25 +0530

    Updated EmployeeDetails.kt program

---
+++ src/main/kotlin/EmployeeDetails.kt
@@ -0,0 +1,23 @@
+package org.example
+
+fun employeeDetails(firstName: String = "DefaultName", lastName: String = "DefaultSurname", age: Int = 25, salary: Double = 70000.0) {
+    functionFileName("Val and Var Program")
+
+    println("Employee details")
+    dottedSeparator()
+    var employeeName : String = "$firstName $lastName"
+    var employeeAge : Int = age
+    var employeeSalary : Double = salary
+
+    val employeeAddress: String = "Bangalore, KA, IN"
+
+//    'val' cannot be reassigned. It will rise an error.
+//    employeeAddress = "Berhampur, OD, IN"
+
+    println("Employee Name: $employeeName")
+    println("Employee Age: $employeeAge")
+    println("Employee Salary: $employeeSalary")
+    println("Employee Address: $employeeAddress")
+
+    dottedSeparator()
+}
