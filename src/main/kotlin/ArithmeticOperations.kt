10f518caf36a543ae481604356d6d76a214918ff
sridharpanda1 <sridharpanda.work@gmail.com>
2026-01-05 23:58:06 +0530

    a=Arithmetic Operations

---
+++ src/main/kotlin/ArithmeticOperations.kt
@@ -0,0 +1,20 @@
+package org.example
+
+fun arithmeticOperations(){
+    functionFileName("Arithmetic Operation Program")
+
+    val x = 20
+    val y = 10
+
+    println("Addition operation: $x + $y = ${x+y} ")
+    dottedSeparator()
+    println("Subtraction operation: $x - $y = ${x-y} ")
+    dottedSeparator()
+    println("Multiplication operation: $x * $y = ${x*y} ")
+    dottedSeparator()
+    println("Division operation: $x / $y = ${x/y} ")
+    dottedSeparator()
+    println("Modulus operation: $x % $y = ${x%y} ")
+    dottedSeparator()
+
+}
