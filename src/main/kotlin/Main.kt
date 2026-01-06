8549f46101eb60110791c39e9b7f756b92e43ec2
sridharpanda1 <sridharpanda.work@gmail.com>
2026-01-07 00:21:49 +0530

    added stringRepetitionOperation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,6 +17,7 @@ fun main() {
     inOperatorOperations()
     bitwiseOperations()
     stringConcatenationOperation()
+    stringRepetitionOperation()
 
 
 }
---
+++ src/main/kotlin/StringRepetition.kt
@@ -0,0 +1,14 @@
+package org.example
+
+fun stringRepetitionOperation() {
+    functionFileName("String Repetition Operation Program")
+
+    val text = "Hi "
+
+    println("String Repetition Operation")
+    dottedSeparator()
+
+    println("text.repeat(3) : ${text.repeat(3)}")
+
+    dottedSeparator()
+}
