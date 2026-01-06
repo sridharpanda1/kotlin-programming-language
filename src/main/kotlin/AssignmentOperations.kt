3e8b817571f6ea6f6d4787811a6f69cca86d1c30
sridharpanda1 <sridharpanda.work@gmail.com>
2026-01-07 00:06:47 +0530

    added assignmentOperations

---
+++ src/main/kotlin/AssignmentOperations.kt
@@ -0,0 +1,27 @@
+package org.example
+
+fun assignmentOperations() {
+    functionFileName("Assignment Operation Program")
+
+    var x = 10
+
+    println("Assignment Operations")
+    dottedSeparator()
+
+    x += 5
+    println("x += 5 : $x")
+
+    x -= 3
+    println("x -= 3 : $x")
+
+    x *= 2
+    println("x *= 2 : $x")
+
+    x /= 4
+    println("x /= 4 : $x")
+
+    x %= 3
+    println("x %= 3 : $x")
+
+    dottedSeparator()
+}
